91b09005aee20f6ad544a5fd8481917932766972
muftiatbakare <bakare.muftiat@gmail.com>
2023-02-10 00:31:00 +0100

    new: stutern
    javascript intro

---
+++ index.js
@@ -0,0 +1,26 @@
+// the question is a bit confusing, so i did it two ways
+// method 1
+
+let a; 
+let b;
+a = 40;
+b = 3;
+let multiplyNumber = (a * b);
+console.log(multiplyNumber);
+
+// method 2
+
+function numberMultiply() {
+    console.log(40*3)
+}
+numberMultiply();
+
+
+let firstName = "Muftiat "
+let lastName = "Bakare Abdullahi"
+let stuternTrack = "FrontEnd"
+let mySkills = "Html, CSS, JavaScript"
+let favoriteColor = "Blue"
+let countryIndependence = "1960"
+ 
+console.log("My name is " + firstName + lastName + ". I am a " + stuternTrack + " developer" + ". You can ask me about " + mySkills + ". My fav color is " + favoriteColor + ". I am a Nigerian and Nigeria got her independence in " + countryIndependence + ". Nice to meet you!");
---
+++ readme.md
@@ -0,0 +1,5 @@
+The code - "node filename.js" runs Javascript outside the browser
+Javascript can
+ECMAScript was first realeased in 1997
+You log to the console using the code "console.log();", click inspect on chrome browser, then click console to view what was logged in the code
+Tech fields that use JavaScript include FrontEnd(React.js), BackEnd(Node.js), Mobile development(React Native)
