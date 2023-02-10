2508609204235086fe26b5354c2ac35478610d68
muftiatbakare <bakare.muftiat@gmail.com>
2023-02-10 01:56:35 +0100

    update

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // the question is a bit confusing, so i did it three ways
-// method 1
-
+// method 1 - Using variable
 // let a; 
 // let b;
 // a = 40;
@@ -8,7 +7,7 @@
 // let multiplyNumber = (a * b);
 // console.log(multiplyNumber);
 
-// // method 2
+// // method 2 -declaring a function but without parameter(s)
 
 // function numberMultiply() {
 //     console.log(40*3)
@@ -23,6 +22,11 @@ numberMultipl(60, 3);
 numberMultipl(6, 50);
 numberMultipl(98, 6);
 
+const numberMulti = function number(number1, number2) {
+    return number1 * number2;
+};
+console.log(numberMulti(6,3));
+console.log(numberMulti(15,30));
 
 let firstName = "Muftiat "
 let lastName = "Bakare Abdullahi"
@@ -32,3 +36,7 @@ let favoriteColor = "Blue"
 let countryIndependence = "1960"
  
 console.log("My name is " + firstName + lastName + ". I am a " + stuternTrack + " developer" + ". You can ask me about " + mySkills + ". My fav color is " + favoriteColor + ". I am a Nigerian and Nigeria got her independence in " + countryIndependence + ". Nice to meet you!");
+
+// or
+
+console.log(`My name is ${firstName} ${lastName}. I am a ${stuternTrack}  developer. You can ask me about ${mySkills}. My fav color is ${favoriteColor}. I am a Nigerian and Nigeria got her independence in ${countryIndependence}. Nice to meet you!`);
